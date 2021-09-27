e2bf8e7e41a95d5687bf7f887826e00881381639
Terry chan <napoler2008@gmail.com>
2021-09-27 20:40:28 +0800

    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-name='tkitDemo'#修改包名字-
-version='0.0.0.2 '
-description='Terry toolkit tkitDemo'
+name='tkitRetransformer'#修改包名字-
+version='0.0.0.21'
+description='Terry toolkit tkitRetransformer'
 author='Terry Chan'
 author_email='napoler2008@gmail.com'
-url='http://www.terrychan.org/python_libs_demo/'
+url='http://www.terrychan.org/tkit-re-transformers/'
 
 copyright = '2021, Terry Chan'
 language = 'zh_cn'
